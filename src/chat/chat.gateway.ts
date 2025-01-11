aaf3085f3afbdcbedcd8be3bce2e9de07da80c7b
Daniel <danielvillalobosfdz@gmail.com>
2025-01-12 00:21:55 +0100

    Add ws-auth middleware

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,11 @@
 import { UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import {
   ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -11,12 +13,21 @@ import {
 
 import { Server, Socket } from 'socket.io';
 import { WsAuthGuard } from 'src/core/auth/guards/ws-auth.guard';
+import { WSAuthMiddleware } from 'src/core/auth/middleware/ws-auth.middleware';
 
 @UseGuards(WsAuthGuard)
 @WebSocketGateway({ namespace: 'chat' })
-export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class ChatGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  constructor(private jwtService: JwtService) {}
+
   @WebSocketServer()
   private server: Server;
+
+  afterInit(server: Server) {
+    server.use(WSAuthMiddleware(this.jwtService));
+  }
 
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
