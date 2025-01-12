7574f1f269b8c9ce3ff9cd28e2416a4445b262ce
Daniel <danielvillalobosfdz@gmail.com>
2025-01-12 19:55:25 +0100

    Remove find-message dto

--- src/chat/dto/find-message.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/mapped-types';
-import { Message } from '../entities/message.entity';
-
-export class FindMessageDto extends PickType(Message, [
-  'receiverId',
-  'senderId',
-] as const) {}
